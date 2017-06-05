19719dfc13b2e58652e483c478c16a8d063c2165
juliahwang <qufskan9396@gmail.com>
2017-06-05 15:10:56 +0900

    add Pizza, Topping models

--- django_app/introduction_to_models/models/__init__.py
+++ django_app/introduction_to_models/models/__init__.py
@@ -1,3 +1,5 @@
 # models 자체에서 불러오고 싶으므로 설정을 등록
 from .car import Car,Manufacturer
 from .person import Person
+from .many_to_one_example import Reporter, Article
+from .topping_and_pizza import Topping, Pizza
