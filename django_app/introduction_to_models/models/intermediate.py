4fc54878a2e51b444151e7540b4a9fe4452c1b50
juliahwang <qufskan9396@gmail.com>
2017-06-05 16:29:28 +0900

    add Intermediate models(Club, Player and TradeInfo)

---
+++ django_app/introduction_to_models/models/intermediate.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Club(models.Model):
+    name = models.CharField(max_length=40)
+    players = models.ManyToManyField(
+        Player,
+        through='TradeInfo',
+        # 아래에 정의했으므로 문자열로 적어준다.
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class TradeInfo(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
+    # 추가 필드가 없으면 기본 many-to-many 모델을 구현한 것이 된다.
+    date_joined = models.DateField()
+    date_left = models.DateField(null=True, blank=True)
