3fa0999d84720e66e410c1b9be16157a74b08f39
juliahwang <qufskan9396@gmail.com>
2017-06-07 12:12:01 +0900

    add practice answer to intermediate models

--- django_app/introduction_to_models/models/intermediate.py
+++ django_app/introduction_to_models/models/intermediate.py
@@ -1,5 +1,7 @@
 from django.db import models
 from datetime import timezone, date
+
+from django.db.models import Q
 from django.utils import timezone
 
 
@@ -12,7 +14,8 @@ class Player(models.Model):
     @property
     def current_club(self):
         """current_club프로퍼티에 현재 속하는 Club리턴"""
-        return self.club_set.get(tradeinfo__date_left__isnull=True).name
+        #return self.club_set.get(tradeinfo__date_left__isnull=True).name
+        return self.current_tradeinfo.club
 
     @property
     def current_tradeinfo(self):
@@ -21,6 +24,7 @@ class Player(models.Model):
             player__exact=self,
             date_joined__lte=timezone.now()
         )
+        # return self.tradeinfo_set.get(date_left__isnull=True)
 
 
 class Club(models.Model):
@@ -40,13 +44,26 @@ class Club(models.Model):
 
     def squad(self, year=None):
         # squad 메서드에 현직 선수들만 리턴
+        # 2015년에 현직이었던 = 2015년에 하루라도 뛰었던 선수
+        # ex) 2015년에 현직으로 존재했던 선수의 경우
+        #  떠난 날짜가 2015. 1. 1 보다는 커야한다
+        #  들어온 날짜는 2016. 1. 1 보다는 작아야 한다.
+        # if year:
+        #
+        #     return '{}'.format(self.players.filter(tradeinfo__date_joined__lte=date(year, 1, 1)))
+        # else:
+        #     return '{}'.format(self.players.filter(tradeinfo__date_joined__lte=timezone.now()))
+        if year:
+            """해당년도가 넘어가기 전에 영입했고 떠난 날이 해당년도를 넘어가거나 떠난 날짜가 없다면."""
+            return self.players.filter(
+                Q(tradeinfo__date_joined__lt=date(year + 1, 1, 1)) &
+                (
+                    Q(tradeinfo__date_left__gt=date(year, 1, 1)) |
+                    Q(tradeinfo__date_left__isnull=True))
+                )
         # 인수로 년도(2017, 2015,... 등)를 받아
         # 해당 년도의 현직 선수들을 리턴
         # 주어지지 않으면 현재를 기준으로 함
-        if not year:
-            return '{}'.format(self.players.filter(tradeinfo__date_joined__lte=date(year, 1, 1)))
-        else:
-            return '{}'.format(self.players.filter(tradeinfo__date_joined__lte=timezone.now()))
 
 
 class TradeInfo(models.Model):
@@ -64,18 +81,30 @@ class TradeInfo(models.Model):
     # 2. recommender와 prev_club을 활성화시키고 Club의 MTM 필드에 through_fields를 명시
     recommender = models.ForeignKey(
         Player,
-        related_name='+',
-        on_delete=models.CASCADE,
+        related_name='tradeinfo_set_by_recommender',
+        # 역참조 매니저의 이름을 오버라이드하여 지정, 사용가능
+        # player.recommender_set.all() = player.tradeinfo_set_by_recommender.all()
+        # related_name = '+'라고 적어주면 역참조할 필요가 없다는 설정임.
+        on_delete=models.PROTECT,
+        # 추천인을 삭제한다고 TradeInfo가 삭제(CASCADE)되면 안되므로 지우지 못하게 PROTECT 사용
         null=True,
         blank=True,
     )
     prev_club = models.ForeignKey(
         Club,
-        related_name='+',
-        on_delete=models.CASCADE,
+        related_name='player_prev_club',
+        on_delete=models.PROTECT,
         null=True,
         blank=True,
     )
+
+    def __str__(self):
+        return '{}, {} ({} ~ {})'.format(
+            self.player,
+            self.club,
+            self.date_joined,
+            self.date_left if self.date_left else '현직'
+        )
 
     @property
     def is_current(self):
@@ -91,9 +120,5 @@ class TradeInfo(models.Model):
                 self.club.name,
             )
 
-    def __str__(self):
-        return '{} from {} joined on {}'.format(
-            self.player,
-            self.club,
-            self.date_joined,
-        )
+            # return self.date_left is None
+            # return not self.date_left
